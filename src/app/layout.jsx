f65e88766156a88ef4d8407ca9e756e7d2765266
Isaac Peña <yzakpm@gmail.com>
2026-08-06 15:49:52 -0600

    feat: Add site metadata, favicons, and web manifest
    Implement essential branding and SEO elements for the application. This includes:
    - Next.js metadata for title and description to improve discoverability.
    - Various favicon sizes and Apple touch icons for consistent branding across devices.
    - A web manifest to enable PWA features and home screen installation.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,11 @@
 import "@/app/globals.css";
 import { ToastProvider } from "@/context/ToastContext";
 
+export const metadata = {
+  title: "Y-Wallet",
+  description: "Administración de tus gastos e ingresos.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
